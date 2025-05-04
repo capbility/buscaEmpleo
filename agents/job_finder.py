0a8006878d29eb49143685165cb7907988d524e9
Jose <pepeisa.ortiz@gmail.com>
2025-05-03 21:53:17 -0500

    settings for gemini and openai

--- agents/job_finder.py
+++ agents/job_finder.py
@@ -1,13 +1,29 @@
 from browser_use import Agent
-from langchain_google_genai import ChatGoogleGenerativeAI
 from browser_use.browser.browser import Browser, BrowserConfig
 from browser_use.browser.context import BrowserContextConfig
-#from config.settings import GEMINI_API_KEY, MODEL
-from config.settings import OPENAI_API_KEY, MODEL
-
 from controllers.job_controller import controller
 import asyncio
-from langchain_openai import ChatOpenAI
+
+#from config.settings import GEMINI_API_KEY, MODEL
+#from langchain_google_genai import ChatGoogleGenerativeAI
+
+#from config.settings import OPENAI_API_KEY, MODEL
+#from langchain_openai import ChatOpenAI
+
+###
+
+from config.settings import PROVIDER, API_KEY, MODEL
+
+if PROVIDER == "gemini":
+    from langchain_google_genai import ChatGoogleGenerativeAI
+    llm = ChatGoogleGenerativeAI(model=MODEL, api_key=API_KEY)
+elif PROVIDER == "openai":
+    from langchain_openai import ChatOpenAI
+    llm = ChatOpenAI(model=MODEL)
+else:
+    raise ValueError("Proveedor no válido. Usa 'gemini' o 'openai'.")
+
+####
 
 browser = Browser(
     config=BrowserConfig(
@@ -17,7 +33,8 @@ browser = Browser(
 
 async def run_job_finder(palabra_clave, es_discapacidad, cantidad_ofertas):
     #llm = ChatGoogleGenerativeAI(model=MODEL, api_key=GEMINI_API_KEY)
-    llm = ChatOpenAI(model=MODEL)
+    #llm = ChatGoogleGenerativeAI(model=MODEL, api_key=API_KEY)
+    #llm = ChatOpenAI(model=MODEL)
     
     diccionario = {
         1: "one",
--- config/settings.py
+++ config/settings.py
@@ -4,12 +4,32 @@ from pydantic import SecretStr
 
 load_dotenv()
 
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+MODEL_GEMINI = os.getenv("MODEL_GEMINI")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL_OPENAI = os.getenv("MODEL_OPENAI")
+
+if GEMINI_API_KEY and MODEL_GEMINI:
+    PROVIDER = "gemini"
+    API_KEY = SecretStr(GEMINI_API_KEY)
+    MODEL = MODEL_GEMINI
+elif OPENAI_API_KEY and MODEL_OPENAI:
+    PROVIDER = "openai"
+    API_KEY = SecretStr(OPENAI_API_KEY)
+    MODEL = MODEL_OPENAI
+else:
+    raise ValueError("No se encontró ninguna configuración válida de modelo. Define las variables en el .env.")
+
+
+"""
 required_env_vars = ['OPENAI_API_KEY']
 for var in required_env_vars:
     if not os.getenv(var):
         raise ValueError(f'{var} is not set. Please add it to your environment variables.')
 
 #GEMINI_API_KEY = SecretStr(os.getenv('GEMINI_API_KEY'))
-#MODEL = os.getenv('MODEL', 'gemini-1.5-flash')
+#MODEL_GEMINI = os.getenv('MODEL', 'gemini-1.5-flash')
 OPENAI_API_KEY = SecretStr(os.getenv('OPENAI_API_KEY'))
-MODEL = os.getenv('MODEL', 'gpt-4o-2024-08-06')
+MODEL = os.getenv('MODEL_OPENAI')
+"""
